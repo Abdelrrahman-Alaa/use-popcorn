fb6935700b53371b8f6bb9957dcd06c2bdfb8e0e
Abdelrahman Alaa <Abdelrrahman.Alaa@gmail.com>
2026-04-11 15:12:12 +0200

    feat: implement StarRating component and update App structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import StarRating from "./StarRating";
+// import './index.css';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <>
+      <StarRating maxRating={10} color="blue" onSetRating={setMovieRating} />
+      <div>
+        <p>This movie has rating {movieRating}</p>
+      </div>
+    </>
+  );
+}
+
+export default Test;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Poor", "Fair", "Good", "Very Good", "Excellent"]}
+    />
+    <Test />
+  </React.StrictMode>,
 );
-
